bb3cdd02c122ce476be3b269f7748344615f63a4
hohoTT <609029365@qq.com>
2017-03-28 21:19:56 +0800

    用实现java.lang.Runnable接口的方式完成java多线程的测试使用用例

---
+++ Java_Multithreading_Case_One/src/com/wt/multithread/UseRunnable.java
@@ -0,0 +1,32 @@
+package com.wt.multithread;
+
+class Thread2 implements Runnable {
+	private String name;
+
+	public Thread2(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println(name + "ÔËÐÐ  :  " + i);
+			try {
+				Thread.sleep((int) Math.random() * 10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
+
+public class UseRunnable {
+
+	public static void main(String[] args) {
+		new Thread(new Thread2("C")).start();
+		new Thread(new Thread2("D")).start();
+	}
+
+}
