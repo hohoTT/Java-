283e82c487550f442ae861c68db1c7902ab3d54a
hohoTT <609029365@qq.com>
2017-03-28 11:43:45 +0800

    添加第一个java多线程的案例，其主要的功能就是实现父子线程的交替运行，项目设定的是父线程为100毫秒，子线程为50毫秒。结果就是父线程运行一次，子线程运行两次

--- Java_Multithreading_Case_One/src/com/wt/test/ThreadTest.java
+++ Java_Multithreading_Case_One/src/com/wt/test/ThreadTest.java
@@ -1,26 +1,56 @@
 package com.wt.test;
 
-public class ThreadTest implements Runnable{
+class NewThead implements Runnable {
 
 	Thread thread;
-	
-	public ThreadTest() {
+
+	public NewThead() {
 
 		thread = new Thread(this, "Demo Thread");
 		System.out.println("Child thread " + thread);
-		
+
 		// 开始线程
-		thread.run();
-		
+		thread.start();
+
 	}
 
 	// 第二个线程的入口
 	public void run() {
 
-		
-		
+		try {
+			for (int i = 1; i <= 5; i++) {
+				System.out.println("Child thread " + i);
+				Thread.sleep(50);
+			}
+		} catch (Exception e) {
+			// 线程中断
+			System.out.println("Child interrupted ");
+		}
+
+		System.out.println("Exiting Child thread ");
 	}
 
-	
-	
+}
+
+public class ThreadTest {
+
+	public static void main(String[] args) {
+
+		// 创建一个新的线程
+		new NewThead();
+
+		try {
+			for (int i = 1; i <= 5; i++) {
+				System.out.println("main thread " + i);
+				Thread.sleep(100);
+			}
+		} catch (Exception e) {
+			// 主线程中断
+			System.out.println("Main thread interrupted ");
+		}
+
+		System.out.println("Exiting Main thread ");
+
+	}
+
 }
